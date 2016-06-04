91046a4c6f0b667245f3180dd9f335f9ce6b38dd
serbanc94 <serbanc94@gmail.com>
2016-06-04 20:41:43 +0300

    commit

--- admin/periodical_points.php
+++ admin/periodical_points.php
@@ -1,1 +1,28 @@
 <?php
+ini_set("display_errors", 0);
+
+$today=date('d-m-Y');
+
+
+$time1 = strtotime('04-06-2016');
+$newformat1 = date('d-m-Y',$time1);
+
+$time2 = strtotime('10-06-2016');
+$newformat2 = date('d-m-Y',$time2);
+
+
+$semester_dates=[
+
+     $newformat1=>"false",
+     $newformat2=>"false"
+
+];
+
+if( false == $newformat1[$today] ){
+
+
+
+
+
+}
+
