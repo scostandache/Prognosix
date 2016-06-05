e9d98db44a768f7994b2ba55b21287d7b9c52d7e
serbanc94 <serbanc94@gmail.com>
2016-06-05 13:19:34 +0300

    commit

--- admin/periodical_points.php
+++ admin/periodical_points.php
@@ -1,10 +1,10 @@
 <?php
 ini_set("display_errors", 0);
 
-$today=date('d-m-Y');
+$today=date('Y-m-d');
 
 
-$time1 = strtotime('04-06-2016');
+/*$time1 = strtotime('04-06-2016');
 $newformat1 = date('d-m-Y',$time1);
 
 $time2 = strtotime('10-06-2016');
@@ -16,34 +16,59 @@ $semester_dates=[
      $newformat1=>false,
      $newformat2=>false
 
-];
+];*/
+$servername="localhost";
+$username="serb_costa";
+$db_pass="pass";
+$DB="TW_database";
+$connection= new mysqli($servername,$username,$db_pass,$DB);
+
+$dates_query="select students_announced from Periodical_announces where announce_date=?";
+$dates_query=$connection->prepare($dates_query);
+$dates_query->bind_param("s",$today );
+$dates_query->execute();
+$dates_query->bind_result( $announced);
+$dates_query->fetch();
+
+mysqli_close($connection);
 
 
-if( false == $semester_dates[$today] ){
-
-    echo "today";
-    $servername = "localhost";
-    $username = "serb_costa";
-    $password = "pass";
-    $dbname = "TW_database";
+if( $announced==0){
+    $connection= new mysqli($servername,$username,$db_pass,$DB);
 
     $grades_query="select student_matricola, luata,ghicita, nume_obiect from note inner join ".
         "evenimente on note.examen_id=evenimente.examen_id inner join ".
         "examene on note.examen_id=examene.id_exam where data_rezultate<CURRENT_DATE";
 
-    $connection = new mysqli($servername, $username, $password, $dbname);
     $students_grades = $connection->query($grades_query);
 
+    mysqli_close($connection);
+
+
     while($row=$students_grades->fetch_assoc()){
-          $row["student_matricola"]["luata"]=789;
+
+        if($row["luata"]!=$row["ghicita"]){
+            $object_name=$row["nume_obiect"];
+            $matricola_student=$row["student_matricola"];
+
+            $update_points_query="UPDATE punctaje set punctaj=punctaj+1 where student_matricola=? and nume_obiect= ?";
+
+            $update_points_query=$connection->prepare($update_points_query);
+            $update_points_query->bind_param("ss",$matricola_student,$object_name );
+
+            
+
+            $connection= new mysqli($servername,$username,$db_pass,$DB);
+
+
+
+        }
+
+
 
 
 
     }
-
-
-
-
 
 }
 
